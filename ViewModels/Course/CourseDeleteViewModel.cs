087695b0080c0e6683de8812c6c6e82066ee0ae6
AAlqahtani47 <93830349+AAlqahtani47@users.noreply.github.com>
2023-06-24 23:05:21 +0300

    Add files via upload

---
+++ ViewModels/Course/CourseDeleteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SeniorProject.ViewModels
+{
+    public class CourseDeleteViewModel
+    {
+        [Required]
+        [Display(Name = "Course ID")]
+        public int Course_Id { get; set; }
+
+        
+        [Display(Name = "Course Title")]
+        public string Course_Title { get; set; }
+
+        
+        [Display(Name = "Course Code")]
+        public string Course_Code{ get; set; }
+
+        
+    }
+}
---
+++ ViewModels/TeachersCourses/TeachersCourseDeleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SeniorProject.Models;
+using System.Collections.Generic;
+
+
+namespace SeniorProject.ViewModels
+{
+    public class TeachersCourseDeleteViewModel
+    {
+        
+            public int teacherCourse_Id { get; set; }
+            public Course course_Ref { get; set; }
+            public Teachers teacher_Ref { get; set; }
+            public String SemesterStart { get; set; }
+            public String SemesterEnd { get; set; }
+        
+    }
+}
